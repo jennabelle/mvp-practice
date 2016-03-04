2ab48f926bb6363eda3892cc450059e9997ac835
Jenna Vuong <jennavuong@gmail.com>
2016-03-03 23:35:22 -0800

    removed hard-coding user credentials

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-        url : 'mongodb://jvuong:telegraph12@ds019038.mlab.com:19038/mvp-trips'
+        // add mongolab db credentials in angle brackets
+        url : 'mongodb://<username>:<password>@ds019038.mlab.com:19038/mvp-trips'
     }
